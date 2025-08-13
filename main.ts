0038e0ae4f3116172f5ea8fa11d2e8727d060c96
9ml <onenpc@qq.com>
2025-08-13 22:34:49 +0800

    配置404页面

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 // server.ts
 import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
 
-Deno.serve((req) => {
-  return serveDir(req, {
+Deno.serve(async (req) => {
+  const res = await serveDir(req, {
     // 静态文件的根目录
     fsRoot: `${Deno.cwd()}/docs`,
     // URL 路径前缀, 默认为空
@@ -13,4 +13,15 @@ Deno.serve((req) => {
     // 是否启用 CORS 默认 false 若为 true，会添加 `Access-Control-Allow-Origin: *` 头
     enableCors: true
   });
+
+  if (res.status === 404) {
+    // 返回自定义 404 页面
+    const notFoundPage = await Deno.readTextFile(`${Deno.cwd()}/docs/404.html`);
+    return new Response(notFoundPage, {
+      status: 404,
+      headers: { "Content-Type": "text/html" },
+    });
+  }
+
+  return res
 });
